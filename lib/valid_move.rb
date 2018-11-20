d7cfbec71eb3fae535a76a4fbeda78bd01f1f5f6
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-20 09:38:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if
+  if position_taken && index.between(0,9)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   !(board[index]=="" || board[index]==" " || baord[index] == nil)
-
 end
